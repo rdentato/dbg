6fad6cc313ab0b1c5fdc39d7be5b62b2b52f5fff
rdentato <rdentato@gmail.com>
2026-07-05 18:53:57 +0200

    fixed dbgvrb()

--- src/dbg.h
+++ src/dbg.h
@@ -48,7 +48,6 @@
 #define _dbgtst DBG_OFF
 #define _dbgtrk DBG_OFF
 #define _dbginf DBG_OFF
-#define _dbgvrb DBG_OFF
 #define _dbgwrn DBG_OFF
 #define _dbgerr DBG_OFF
 #define _dbgchk DBG_OFF
@@ -56,6 +55,7 @@
 #define _dbgptr DBG_OFF
 #define _dbgclk DBG_OFF
 #define _dbgblk while(0)
+#define _dbgvrb(...) __VA_ARGS__
 
 // ## No DEBUG
 // If DEBUG is not defined, the dbgxxx symbol should still be defined,
